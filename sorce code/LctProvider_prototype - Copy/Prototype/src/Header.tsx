186cf57884ee33c03955545f839ece0b58a96895
Maryjoy Nyagah <nyagahmaryjoy2@gmail.com>
2025-01-08 16:41:06 +0300

    healthcare Project
    update

--- sorce code/LctProvider_prototype - Copy/Prototype/src/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header(){
-    return (
-        <div>
-            <h1 className="text-4x1 text-white font-bold text-center  mb-5" >HEALTH CARE CENTER</h1>
-        </div>
-    );
-}
